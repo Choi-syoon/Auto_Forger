cb07f8edfee72383fecfa6a9c10e08b6da6599df
neulett <neulet.git@gmail.com>
2024-05-25 18:35:34 +0900

    video - keypoints_extraction 추가

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import os
 import cv2
 import math
 import numpy as np
+import mediapipe as mp
 
 class dataset:
     def __init__(self):
@@ -12,7 +13,72 @@ class dataset:
     class Video:
         def __init__(self) -> None:
             pass
-        
+
+        def keypoint_extraction(self, filename):
+            full_filename = os.path.join(self.PADDED_SAVE_PATH, filename)
+            cap = cv2.VideoCapture(full_filename)
+
+            if self.MODEL == "pose":
+                solution = mp.solutions.pose.Pose
+            elif self.MODEL == "holistic":
+                solution = mp.solutions.holistic.Holistic
+            else:
+                raise ValueError("Invalid Model")
+            with solution(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+
+                    while True:
+                        opened, image = cap.read()
+                        if not opened:
+                            break
+
+                        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                        results = pose.process(image_rgb)
+
+                        if self.MODEL == "pose" and results.pose_landmarks:
+                            for landmark in results.pose_landmarks.landmark:
+                                self.keypoints.append([landmark.x, landmark.y, landmark.z, landmark.visibility])
+                            self.keypoints_list.append(self.keypoints)
+
+                            np.save(f"{self.FEATURE_SAVE_PATH}/{os.path.splitext(os.path.basename(filename))[0]}_pose.npy", self.keypoints_list) 
+
+                        elif self.MODEL == "holistic" and (results.face_landmarks or
+                                                      results.left_hand_landmarks or
+                                                      results.right_hand_landmarks or
+                                                      results.pose_landmarks):
+                            keypoints = {'face': [],
+                                         'left_hand': [],
+                                         'right': [],
+                                         'pose': []}
+
+                            if results.pose_landmarks:
+                                for landmark in results.pose_landmarks.landmark:
+                                    keypoints['pose'].append([
+                                        landmark.x, landmark.y, landmark.z
+                                    ])
+
+                            if results.face_landmarks:
+                                for landmark in results.face_landmarks.landmark:
+                                    keypoints['face'].append([
+                                        landmark.x, landmark.y, landmark.z
+                                    ])
+
+                            if results.left_hand_landmarks:
+                                for landmark in results.left_hand_landmarks.landmark:
+                                    keypoints['left_hand'].append([
+                                        landmark.x, landmark.y, landmark.z
+                                    ])
+
+                            if results.right_hand_landmarks:
+                                for landmark in results.right_hand_landmarks.landmark:
+                                    keypoints['right_hand'].append([
+                                        landmark.x, landmark.y, landmark.z
+                                    ])
+
+                            self.keypoints_list.append(keypoints)
+                            np.save(f"{self.FEATURE_SAVE_PATH}/{os.path.splitext(os.path.basename(filename))[0]}_holistic.npy", self.keypoints_list)
+
+                    cap.release()
+
         class Pad:
             def video_length(self, mode=""):    # min, max mode
                 current_len = 0
@@ -38,3 +104,5 @@ class dataset:
                         target_file = filename
 
                 return target_len, target_file
+            
+        
